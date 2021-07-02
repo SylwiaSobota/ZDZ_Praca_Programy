7496da769f5b1240f77330ff4d29c6099abd495c
SylwiaSobota <32569088+SylwiaSobota@users.noreply.github.com>
2021-07-02 13:54:34 +0200

    Update baza.sql

--- Egzaminy praktyczne/E.14/e-14-18-01-01/baza.sql
+++ Egzaminy praktyczne/E.14/e-14-18-01-01/baza.sql
@@ -1,1 +1,11 @@
 --odpowiedzi dotyczace baz danych
+--zadanie1
+SELECT id, tytul, tresc FROM ogloszenie WHERE kategoria = 1
+--zadanie2
+SELECT telefon FROM uzytkownik JOIN ogloszenie ON ogloszenie.uzytkownik_id = uzytkownik.id WHERE ogloszenie.id = 1 
+--zadanie2 inna wersja
+SELECT telefon FROM uzytkownik, ogloszenie WHERE ogloszenie.uzytkownik_id = uzytkownik.id AND ogloszenie.id = 1
+--zadanie3
+CREATE USER 'moderator'@'localhost' IDENTIFIED BY 'qwerty'
+--zadanie4
+GRANT SELECT, DELETE ON ogloszenie TO 'moderator'@'localhost'
