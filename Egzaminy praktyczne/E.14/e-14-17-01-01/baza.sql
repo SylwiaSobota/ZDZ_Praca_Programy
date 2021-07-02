2a7ebca3f5655e41c7ac4e2d573e7e2a3ccdc871
SylwiaSobota <32569088+SylwiaSobota@users.noreply.github.com>
2021-07-02 13:54:27 +0200

    Update baza.sql

--- Egzaminy praktyczne/E.14/e-14-17-01-01/baza.sql
+++ Egzaminy praktyczne/E.14/e-14-17-01-01/baza.sql
@@ -1,1 +1,7 @@
 --odpowiedzi dotyczace baz danych
+--zadanie1
+SELECT id, marka, model FROM samochody
+--zadanie2
+ SELECT Samochody_id, Klient FROM zamowienia
+ --zadanie3
+ SELECT * FROM samochody WHERE marka = 'Fiat' 
