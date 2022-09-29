607ee5f43bcbdbebc73e099a8fb63bdf919c43fd
SylwiaSobota <32569088+SylwiaSobota@users.noreply.github.com>
2022-09-29 13:16:52 +0200

    Create index.php

---
+++ Zadania/2022-2023/Web/PHP/Zaj1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Strona z PHP</title>
+        <style>@import url('styl.css');</style>
+    </head>
+    <body>
+        <?php 
+        $zm ="Witaj w PHP";
+        echo "<h1>".$zm."</h1>";
+        /*Wykonaj dodawanie, odejmowanie, mnozenie i dzielenie na liczbach a i b. Wyniki wyświetl w paragrafach*/ 
+        $a = 18;
+        $b = 29;
+        $suma = $a+$b;
+        echo "<p>Suma wynosi: ".$suma."</p>";
+
+        /*Utwórz tablicę 10 elementową z losowo przez siebie wybranymi wartościami*/ 
+        
+
+        ?>
+    </body>
+</html>
