a92766002cdfbe54f046e38eba6ef556886ea9d1
SylwiaSobota <sylwia2238@gmail.com>
2023-08-27 21:25:36 +0200

    Delete baza.sql

--- Egzaminy praktyczne/E.14/e-14-17-01-01/baza.sql
+++
@@ -1,7 +0,0 @@
---odpowiedzi dotyczace baz danych
---zadanie1
-SELECT id, marka, model FROM samochody
---zadanie2
- SELECT Samochody_id, Klient FROM zamowienia
- --zadanie3
- SELECT * FROM samochody WHERE marka = 'Fiat' 
