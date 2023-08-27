9216428b45a1e4e36e06d1e261307c44072b4d41
SylwiaSobota <sylwia2238@gmail.com>
2023-08-27 21:26:38 +0200

    Delete baza.sql

--- Egzaminy praktyczne/E.14/e-14-19-06-01/baza.sql
+++
@@ -1,9 +0,0 @@
---odpowiedzi dotyczace baz danych
---zadanie1
- SELECT nazwa FROM towary WHERE promocja=1
---zadanie2
- SELECT cena FROM towary WHERE nazwa='cienkopis' 
---zadanie3
-SELECT dostawcy.nazwa, COUNT(towary.nazwa) FROM dostawcy INNER JOIN towary ON dostawcy.id=towary.idDostawcy GROUP BY dostawcy.nazwa
---zadanie4
-ALTER TABLE dostawcy ADD e_mail TEXT
